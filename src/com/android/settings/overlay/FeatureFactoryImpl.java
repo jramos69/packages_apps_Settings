ea5f20d5c58cde1cbba0e8c5353e24a27b120662
Juan <jramos69@gmail.com>
2022-10-14 21:14:40 -0400

    Import AccountFeatureProviderGoogle
    Change-Id: If078fe1bf59e5c92eb84a1b3ebb667b39a807303

--- src/com/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/android/settings/overlay/FeatureFactoryImpl.java
@@ -74,6 +74,7 @@ import com.android.settings.users.UserFeatureProviderImpl;
 import com.android.settings.wifi.WifiTrackerLibProvider;
 import com.android.settings.wifi.WifiTrackerLibProviderImpl;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
+import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 
 /**
  * {@link FeatureFactory} implementation for AOSP Settings.
@@ -257,7 +258,7 @@ public class FeatureFactoryImpl extends FeatureFactory {
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
         if (mAccountFeatureProvider == null) {
-            mAccountFeatureProvider = new AccountFeatureProviderImpl();
+            mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
         }
         return mAccountFeatureProvider;
     }
